0be53818e9b39cef4224f7a06598e5b2ba47fc4e
Brian <briantcheung@gmail.com>
2024-11-28 01:30:44 -0800

    Added styled components

---
+++ src/components/_styled/StyledTable.tsx
@@ -0,0 +1,19 @@
+import { styled, TableRow, TableCell, tableCellClasses } from "@mui/material";
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    backgroundColor: theme.menu.default,
+    "&: hover": {
+        backgroundColor: theme.menu.hover,
+    },
+    "&:last-child td, &:last-child th": {
+        border: 0,
+    },
+}));
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    borderColor: theme.border.color,
+    padding: "8px 16px",
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.background(8),
+    },
+}));
