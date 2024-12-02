16c371157e55e0ac763552c110d203e242184438
Brian <briantcheung@gmail.com>
2024-12-01 16:39:03 -0800

    Clean up bottom nav

--- src/components/nav/NavBottom.tsx
+++ src/components/nav/NavBottom.tsx
@@ -23,52 +23,54 @@ function NavBottom() {
     const theme = useTheme();
     const matches_down_sm = useMediaQuery(theme.breakpoints.down("sm"));
 
+    const iconButtonStyle: SxProps = {
+        px: "4px",
+    };
+
     const svgStyle: SxProps = {
-        width: { xs: "24px", sm: "32px" },
-        height: { xs: "24px", sm: "32px" },
-        mt: { xs: "2.5px", sm: 0 },
+        width: { xs: "22px", sm: "24px" },
+        height: { xs: "22px", sm: "24px" },
     };
 
     return (
         <AppBar
             position="relative"
-            sx={{ borderWidth: "1px 0 0 1px", zIndex: theme.zIndex.drawer + 1 }}
+            sx={{
+                borderWidth: matches_down_sm ? "1px 0 0 0" : "1px 0 0 1px",
+                zIndex: theme.zIndex.drawer + 1,
+            }}
         >
             <Toolbar
                 sx={{
-                    justifyContent: "center",
-                    display: { xs: "block", sm: "flex" },
-                    my: "10px",
+                    justifyContent: "space-between",
+                    display: { xs: "block", md: "flex" },
+                    py: "10px",
                 }}
             >
-                <TextStyled variant="body2" gutterBottom>
+                <TextStyled variant="body2">
                     IRMINSUL.GG is not affiliated with HoYoverse.
                     <br />
                     Game contents are trademarks and copyrights of HoYoverse.
                 </TextStyled>
-                <Box sx={{ display: { xs: "block", sm: "flex" } }}>
+                <Box sx={{ display: { xs: "block", md: "flex" } }}>
                     <Divider
-                        orientation={
-                            !matches_down_sm ? "vertical" : "horizontal"
-                        }
-                        variant="middle"
-                        flexItem
+                        orientation="horizontal"
                         sx={{
-                            mx: { xs: 0, sm: "25px" },
-                            my: { xs: "10px", sm: 0 },
+                            display: { xs: "block", md: "none" },
+                            my: "10px",
                         }}
                     />
                     <Box sx={{ display: "flex" }}>
                         <Box
                             sx={{
+                                display: { xs: "flex", md: "block" },
                                 alignItems: "center",
-                                display: { xs: "flex", sm: "block" },
                             }}
                         >
                             <TextStyled
                                 variant="body2"
-                                gutterBottom
                                 align="center"
+                                sx={{ mr: { xs: "8px", md: 0 } }}
                             >
                                 GitHub:
                             </TextStyled>
@@ -77,83 +79,76 @@ function NavBottom() {
                                 href="https://github.com/bcheung98/project-phaethon"
                                 target="_blank"
                                 color="inherit"
-                                sx={{ pr: { xs: "0px", sm: "8px" } }}
+                                sx={{ display: "flex", mx: "auto", px: "4px" }}
                             >
-                                <GitHubIcon
-                                    sx={{
-                                        fontSize: { xs: 20, sm: 28 },
-                                        mt: { xs: "-2.5px", sm: 0 },
-                                    }}
-                                />
+                                <GitHubIcon sx={svgStyle} />
                             </IconButton>
                         </Box>
                         <Divider
                             orientation="vertical"
-                            variant="middle"
                             flexItem
-                            sx={{
-                                mx: { xs: "10px", sm: "25px" },
-                                my: 0,
-                            }}
+                            sx={{ mx: { xs: "10px", md: "25px" } }}
                         />
                         <Box
                             sx={{
+                                display: { xs: "flex", md: "block" },
                                 alignItems: "center",
-                                display: { xs: "flex", sm: "block" },
                             }}
                         >
                             <TextStyled
                                 variant="body2"
-                                gutterBottom
                                 align="center"
+                                sx={{ mr: { xs: "8px", md: 0 } }}
                             >
                                 Made with:
                             </TextStyled>
-                            <IconButton
-                                disableRipple
-                                href="https://vite.dev"
-                                target="_blank"
-                                color="inherit"
-                                sx={{ pr: { xs: "0px", sm: "8px" } }}
-                            >
-                                <Avatar
-                                    variant="square"
-                                    src={viteLogo}
-                                    className="logo"
-                                    alt="Vite logo"
-                                    sx={svgStyle}
-                                />
-                            </IconButton>
-                            <IconButton
-                                disableRipple
-                                href="https://react.dev"
-                                target="_blank"
-                                color="inherit"
-                                sx={{ pr: { xs: "0px", sm: "8px" } }}
-                            >
-                                <Avatar
-                                    variant="square"
-                                    src={reactLogo}
-                                    className="logo"
-                                    alt="Vite logo"
-                                    sx={svgStyle}
-                                />
-                            </IconButton>
-                            <IconButton
-                                disableRipple
-                                href="https://mui.com"
-                                target="_blank"
-                                color="inherit"
-                                sx={{ pr: { xs: "0px", sm: "8px" } }}
-                            >
-                                <Avatar
-                                    variant="square"
-                                    src={muiLogo}
-                                    className="logo"
-                                    alt="Vite logo"
-                                    sx={svgStyle}
-                                />
-                            </IconButton>
+                            <Box>
+                                <IconButton
+                                    disableRipple
+                                    href="https://vite.dev"
+                                    target="_blank"
+                                    color="inherit"
+                                    sx={iconButtonStyle}
+                                >
+                                    <Avatar
+                                        variant="square"
+                                        src={viteLogo}
+                                        className="logo"
+                                        alt="Vite logo"
+                                        sx={svgStyle}
+                                    />
+                                </IconButton>
+                                <IconButton
+                                    disableRipple
+                                    href="https://react.dev"
+                                    target="_blank"
+                                    color="inherit"
+                                    sx={iconButtonStyle}
+                                >
+                                    <Avatar
+                                        variant="square"
+                                        src={reactLogo}
+                                        className="logo"
+                                        alt="Vite logo"
+                                        sx={svgStyle}
+                                    />
+                                </IconButton>
+                                <IconButton
+                                    disableRipple
+                                    href="https://mui.com"
+                                    target="_blank"
+                                    color="inherit"
+                                    sx={iconButtonStyle}
+                                >
+                                    <Avatar
+                                        variant="square"
+                                        src={muiLogo}
+                                        className="logo"
+                                        alt="Vite logo"
+                                        sx={svgStyle}
+                                    />
+                                </IconButton>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
