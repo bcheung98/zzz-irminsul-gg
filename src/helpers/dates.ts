17c8954fa5b89744c53b8f4143b78dcf0a8f4e1a
Brian <briantcheung@gmail.com>
2024-12-02 23:36:15 -0800

    Create case for invalid date string input

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -16,14 +16,22 @@ const months = [
 const offset = ["-5", "+1", "+8"]; // NA, EU, Asia
 
 export function createDateObject(date: string) {
-    const formatDate = (date: string, offset: string) =>
-        `${date.split(" ").slice(0, 2).join(" ").replace(/-/g, "/")}${offset}`;
+    let dateObj, dateString, timestamp;
+    if (date) {
+        const formatDate = (date: string, offset: string) =>
+            `${date
+                .split(" ")
+                .slice(0, 2)
+                .join(" ")
+                .replace(/-/g, "/")}${offset}`;
 
-    let dateObj;
-    if (date.endsWith("UTC+8")) {
-        dateObj = new Date(formatDate(date, "+8"));
+        if (date.endsWith("UTC+8")) {
+            dateObj = new Date(formatDate(date, "+8"));
+        } else {
+            dateObj = new Date(formatDate(date, offset[0]));
+        }
     } else {
-        dateObj = new Date(formatDate(date, offset[0]));
+        dateObj = new Date();
     }
 
     const arr = dateObj.toLocaleString().split(",");
@@ -32,14 +40,16 @@ export function createDateObject(date: string) {
     const month = months[Number(dateArr[0]) - 1];
     const day = parseInt(dateArr[1], 10).toString();
     const year = dateArr[2];
-    const dateString = `${month} ${day}, ${year}`;
+    dateString = `${month} ${day}, ${year}`;
 
     const timeArr = arr[1].trim().split(" ");
-    const timestamp = `${timeArr[0].split(":").splice(0, 2).join(":")} ${
-        timeArr[1]
-    }`;
+    timestamp = `${timeArr[0].split(":").splice(0, 2).join(":")} ${timeArr[1]}`;
 
-    return { obj: dateObj, date: dateString, time: timestamp };
+    return {
+        obj: dateObj,
+        date: dateString,
+        time: timestamp,
+    };
 }
 
 export function isCurrentBanner(start: Date, end: Date) {
