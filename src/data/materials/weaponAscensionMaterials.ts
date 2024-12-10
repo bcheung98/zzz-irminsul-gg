b789641321056a4e69fab72338628d6c8b05432d
Brian <briantcheung@gmail.com>
2024-12-09 16:55:23 -0800

    Added more materials

---
+++ src/data/materials/weaponAscensionMaterials.ts
@@ -0,0 +1,29 @@
+export const weaponAscensionMaterials = {
+    Attack: {
+        Attack1: "Attack Component",
+        Attack2: "Reinforced Attack Component",
+        Attack3: "Specialized Attack Component",
+    },
+    Stun: {
+        Stun1: "Stun Component",
+        Stun2: "Reinforced Stun Component",
+        Stun3: "Specialized Stun Component",
+    },
+    Anomaly: {
+        Anomaly1: "Anomaly Component",
+        Anomaly2: "Reinforced Anomaly Component",
+        Anomaly3: "Specialized Anomaly Component",
+    },
+    Defense: {
+        Defense1: "Defense Component",
+        Defense2: "Reinforced Defense Component",
+        Defense3: "Specialized Defense Component",
+    },
+    Support: {
+        Support1: "Support Component",
+        Support2: "Reinforced Support Component",
+        Support3: "Specialized Support Component",
+    },
+};
+
+export const materialRarity = [2, 4];
