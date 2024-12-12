7a288099370777f4325f816e468705c50984321e
Brian <briantcheung@gmail.com>
2024-12-11 23:29:06 -0800

    Rename keys

--- src/data/materials/xpMaterials.ts
+++ src/data/materials/xpMaterials.ts
@@ -1,13 +1,13 @@
 export const characterXPMaterials = {
-    characterXP1: "Trainee Investigator Log",
-    characterXP2: "Official Investigator Log",
-    characterXP3: "Senior Investigator Log",
+    CharacterXP1: "Trainee Investigator Log",
+    CharacterXP2: "Official Investigator Log",
+    CharacterXP3: "Senior Investigator Log",
 };
 
 export const weaponXPMaterials = {
-    weaponXP1: "W-Engine Battery",
-    weaponXP2: "W-Engine Power Supply",
-    weaponXP3: "W-Engine Energy Module",
+    WeaponXP1: "W-Engine Battery",
+    WeaponXP2: "W-Engine Power Supply",
+    WeaponXP3: "W-Engine Energy Module",
 };
 
 export const materialRarity = [2, 4];
