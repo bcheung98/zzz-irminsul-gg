9445343c398415d39d17be057118da0126a974cc
Brian <briantcheung@gmail.com>
2024-12-12 23:01:31 -0800

    Added WIP banner

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,27 @@
-import VersionHighlights from "./VersionHighlights"
+import VersionHighlights from "./VersionHighlights";
+import { TextStyled } from "styled/StyledTypography";
+import { Card } from "@mui/material";
 
 function Home() {
-
-    document.title = `Zenless Zone Zero ${import.meta.env.VITE_DOCUMENT_TITLE}`
+    document.title = `Zenless Zone Zero ${import.meta.env.VITE_DOCUMENT_TITLE}`;
 
     return (
         <>
+            <Card
+                sx={(theme) => ({
+                    backgroundColor: theme.background(3),
+                    mb: "20px",
+                    p: "25px",
+                })}
+            >
+                <TextStyled>
+                    Hello! This site is currently WIP. Features may be missing
+                    or broken.
+                </TextStyled>
+            </Card>
             <VersionHighlights />
         </>
-    )
-
+    );
 }
 
-export default Home
+export default Home;
