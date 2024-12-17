edefedb88a049447078043551f417ab413e5191b
Brian <briantcheung@gmail.com>
2024-12-16 16:45:23 -0800

    Added new version

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -1,5 +1,6 @@
 // MAKE SURE TO CHANGE THIS EVERY UPDATE!
 export const updates = [
+    { version: "1.4", name: "A Storm of Falling Stars" },
     { version: "1.3", name: "Virtual Revenge" },
     { version: "1.2", name: "Tour de Inferno" },
     { version: "1.1", name: "Undercover R&B" },
