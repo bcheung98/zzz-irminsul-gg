4f860e60bc163d5f7bbea4476d45f5c8f6238563
Brian <briantcheung@gmail.com>
2024-12-26 11:46:33 -0800

    Adjust width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -26,7 +26,7 @@ function Layout() {
             <Box id="back-to-top-anchor" />
             <Box sx={{ display: "flex" }}>
                 <Nav />
-                <Box sx={{ width: "100vw" }}>
+                <Box sx={{ minWidth: "50vw", width: "100vw" }}>
                     <Box
                         sx={{
                             px: "24px",
