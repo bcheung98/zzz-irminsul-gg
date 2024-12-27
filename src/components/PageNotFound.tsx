2091301172746f1561cd24a5dfe2b6ede7156e5d
Brian <briantcheung@gmail.com>
2024-12-26 23:37:01 -0800

    Update 404 page

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,10 +1,39 @@
-import { TextStyled } from "styled/StyledTypography";
+import { Link } from "react-router";
+
+// Component imports
+import Image from "custom/Image";
+import { Text, TextStyled } from "styled/StyledTypography";
+
+// MUI imports
+import { useTheme, Stack, Button, getContrastRatio } from "@mui/material";
 
 function PageNotFound() {
+    const theme = useTheme();
+
     return (
-        <TextStyled variant="h5-styled" className="page-name">
-            Page Not Found
-        </TextStyled>
+        <Stack alignItems="center" spacing={2}>
+            <Image src="emotes/Error" alt="404" style={{ width: "256px" }} />
+            <TextStyled variant="h4-styled">404</TextStyled>
+            <Text variant="h4">
+                The page you were looking for was not recorded in Irminsul.
+            </Text>
+            <Button component={Link} to="/" variant="contained">
+                <TextStyled
+                    variant="h6-styled"
+                    sx={{
+                        color:
+                            getContrastRatio(
+                                theme.palette.primary.main,
+                                theme.text.primary
+                            ) > 4.5
+                                ? theme.text.primary
+                                : theme.text.contrast,
+                    }}
+                >
+                    Back to Home
+                </TextStyled>
+            </Button>
+        </Stack>
     );
 }
 
