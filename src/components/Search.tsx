4429466f0cff4f735ffb075978ac8c157b253f1c
Brian <briantcheung@gmail.com>
2025-01-28 23:24:54 -0800

    Add dep array

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -228,7 +228,7 @@ function Search() {
 
     useEffect(() => {
         window.addEventListener("keydown", keyDownHandler);
-    });
+    }, []);
 
     const subHeaderTextStyle = {
         alignItems: "center",
