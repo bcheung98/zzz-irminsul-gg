67625112fe822a99ef29f1d7ab0448055fadda66
Brian <briantcheung@gmail.com>
2025-10-13 13:57:08 -0700

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths()
-    ],
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        if (id.includes("@mui")) {
+                            return "vendor_mui";
+                        }
+                        return "vendor";
+                    }
+                },
+            },
+        },
+    },
+    plugins: [react(), tsconfigPaths()],
+});
